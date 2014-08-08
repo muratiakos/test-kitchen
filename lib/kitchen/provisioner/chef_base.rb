40cbb49fe1192176098deea348b8eb84779ee06f
afiune <salim@afiunemaya.com.mx>
2014-08-08 14:18:24 -0500

    Enabled Busser with Transport shell logic.
    Added default_port and default max_threads parameters.
    Modified chef_omnibus functions with a nice progress bar.
    Deleted old winrm.rb and winrm_base.rb from project.

--- lib/kitchen/provisioner/chef_base.rb
+++ lib/kitchen/provisioner/chef_base.rb
@@ -99,7 +99,7 @@ module Kitchen
 	          path = File.join(config[:root_path], dir)
 	          cmd = "if ( Test-Path #{path} ) { rm -r #{path} };"
 	        end
-        	lines = [dirs, "if (-Not (Test-Path #{config[:root_path]})) { mkdir #{config[:root_path]} }"]
+        	lines = [dirs, "if (-Not (Test-Path #{config[:root_path]})) { mkdir #{config[:root_path]} | Out-Null }"]
         else 
           raise "Unsupported shell: #{shell}"
         end
@@ -155,10 +155,10 @@ module Kitchen
         else 
           raise "[chef_shell_helpers] Unsupported shell: #{shell}"
         end
-        
+
         IO.read(File.join(
           File.dirname(__FILE__), %W[.. .. .. support #{file}]
-        ))
+        )).gsub(/\\n/, "\n")
       end
 
       # Generates the shell code to conditionally install a Chef Omnibus
